d2246a1e40355a65026d3accc500abefceb118b4
ange47rm <ange47rm@hotmail.com>
2022-06-15 09:06:23 +0100

    Revert "CRUD operations now working and UI completed with notifications."
    This reverts commit a89a21b4fd5f3bcf49746a7c8d9f9beb4249c16d.

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -5,13 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// builder.Services.AddRazorPages();
 
 // The below establishes a connection to the Db (it picks up the Connection String from appsettings.json).
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));;
-
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
